b46f65fc186d57eb79a87f3881c6ebf19ffe8071
Ubuntu <ubuntu@ubuntu.fritz.box>
2022-09-24 13:31:56 +0200

    improved security

--- backup/mysql_dump.sh
+++ backup/mysql_dump.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 #Backup Script
 
+## GLOBAL VARS
     DATABASEUSER="root" # Datenbankbenutzer
     LVNAME="/dev/backup/mysql" # zu sicherndes Verzeichnis
     BACKUP_DIR="/media/mysql"
 
+## MOUNT
     umount $LVNAME
     mount $LVNAME $BACKUP_DIR
-		cd $BACKUP_DIR
-    db_ip=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mariadb`
-    db_login="-h$db_ip -u$DATABASEUSER -p$(cat /home/ubuntu/docker/secrets/mysql_root_password)"
-		for DB in $(mysql $db_login -e 'show databases' -s --skip-column-names); do
-				mysqldump $DB > "./$DB-$(date +%Y%m%d).sql" $db_login --skip-lock-tables --column-statistics=0 --compact
-		done
-		bak_files=`ls ./*.sql`
-		for bak in $bak_files; do
-						bak_name=`echo $bak`
-						bak_date=`echo $bak_name | cut -d'-' -f2- | cut -d'.' -f1`
-						if [[ "$bak_date" < "$(date +%Y%m%d -d "30 days ago")" ]] ; then
-										rm $bak
-						fi
-		done
+    cd $BACKUP_DIR
+
+## CREATE CREDENTIALS FILE
+		credentialsFile=./mysql-credentials.cnf
+		echo "[client]" > $credentialsFile
+		echo "user=$DATABASEUSER" >> $credentialsFile
+		echo "password=$(cat /home/ubuntu/docker/secrets/mysql_root_password)" >> $credentialsFile
+		echo "host=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mariadb)" >> $credentialsFile
+
+## DUMP ALL DATABASES
+    for DB in $(mysql --defaults-extra-file=$credentialsFile -e 'show databases' -s --skip-column-names); do
+        mysqldump --defaults-extra-file=$credentialsFile --skip-lock-tables --column-statistics=0 --compact $DB | gzip > "./$DB-$(date +%Y%m%d).sql.gz"
+    done
+    rm ./mysql-credentials.cnf
+
+## ROTATE >30 DAYS
+    bak_files=`ls ./*.sql.gz`
+    for bak in $bak_files; do
+        bak_name=`echo $bak`
+        bak_date=`echo $bak_name | cut -d'-' -f2- | cut -d'.' -f1`
+        if [[ "$bak_date" < "$(date +%Y%m%d -d "30 days ago")" ]] ; then
+                rm $bak
+        fi
+    done
+
+## UNMOUNT
     cd /
     umount $LVNAME
 
